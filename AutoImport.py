9f4fe5b3efa08862495556664a8feaca461fe4b2
hkokora <30753795+hkokora@users.noreply.github.com>
2020-04-06 15:42:04 -0400

    Update and rename 1Test.py to AutoImport.py

--- AutoImport.py
+++ AutoImport.py
@@ -2,7 +2,7 @@ from StaffBase import AutoImport
 
 token = 'Enter Yours'
 mappings = 'externalID,profile-field:firstName,profile-field:lastName,emails,,role,profile-field:avatar'
-csv_location = './hansassessment.csv'
+csv_location = 'path to the CSV file'
 new_auto_import = AutoImport(token=token, csv=csv_location, mappings=mappings)
 new_auto_import.upload_csv()
 new_auto_import.update_csv_mappings(debug=True)
